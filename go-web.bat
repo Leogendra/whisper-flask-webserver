51cecb5a16a844a741eaa9adbb73f225785217a2
Leogendra <le.magicien.annonyme@gmail.com>
2025-11-20 22:50:37 +0100

    Fix launcher

--- go-web.bat
+++ go-web.bat
@@ -1,9 +1,10 @@
 @echo off
 call venv\Scripts\activate
 
+start http://localhost:5000
+
 @REM The -u option forces the stdout and stderr streams to be unbuffered.
 python -u app.py
 
-start http://localhost:5000
 
 pause
